89f2e94183ea5a2628f7c0579a59f206c9ad6cce
birju0451 <165183340+birju0451@users.noreply.github.com>
2026-06-23 01:31:46 +0530

    Initial commit

---
+++ src/main/java/com/birju/spring_boot_rest/JobRestController.java
@@ -0,0 +1,30 @@
+package com.birju.spring_boot_rest;
+import com.birju.spring_boot_rest.model.JobPost;
+import com.birju.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class JobRestController {
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    @ResponseBody
+    public List<JobPost> getAllJob(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId){
+        return service.getJob(postId);
+    }
+
+    @PostMapping("jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost){
+        return service.addJob(jobPost);
+    }
+
+}

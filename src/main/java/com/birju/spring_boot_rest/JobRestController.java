d64cd5cf19f95bd678c1dc5c1288ecee3737a288
birju0451 <165183340+birju0451@users.noreply.github.com>
2026-09-08 16:25:23 +0530

    first commit

--- src/main/java/com/birju/spring_boot_rest/JobRestController.java
+++ src/main/java/com/birju/spring_boot_rest/JobRestController.java
@@ -24,7 +24,7 @@ public class JobRestController {
     }
 
     @GetMapping("jobPosts/keyword/{keyword}")
-    public List<JobPost> serchByKeyword(@PathVariable("keyword") String keyword){
+    public List<JobPost> searchByKeyword(@PathVariable("keyword") String keyword){
         return  service.search(keyword);
     }
     @PostMapping("jobPost")
